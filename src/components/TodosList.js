a6c893833227b99dee227be9bf551a8d025bf7dc
Tom Thornton <thomas.thornton92@gmail.com>
2020-11-18 10:41:00 +0000

    first commit

---
+++ src/components/TodosList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import TodoItem from './TodoItem'
+
+
+class TodosList extends React.Component {
+
+    render() {
+        return (
+            <>
+              {this.props.todos.map(todo => (
+                  <TodoItem key={todo.id} todo={todo} handleChangeProps={this.props.handleChangeProps} deleteTodosProps={this.props.deleteTodoProps}/>
+              ))}
+            </>
+        )
+    }
+}
+export default TodosList
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// component imports
+import TodoContainer from './components/TodoContainer'
+import "./App.css"
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <TodoContainer/>,
   document.getElementById('root')
 );
 
